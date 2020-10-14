34d191b6d6611c4f669d19a471f49e3ee36f5c7f
SvetlanaReutskaya <lana.reutska@gmail.com>
2020-10-14 13:15:47 +0300

    feat: solve problem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  let nStr = n.toString();
+
+  let newString = "";
+    for (let i = nStr.length - 1; i >= 0; i--) {
+        newString += nStr[i];
+    }
+
+  if(n < 0) newString = newString.substring(0, newString.length - 1);
+  return Number(newString);
 }
